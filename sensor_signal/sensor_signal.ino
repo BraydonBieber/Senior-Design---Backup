119332191bd2162b3afb91955cb175b4db023e89
BraydonBieber <90486545+BraydonBieber@users.noreply.github.com>
2021-11-14 02:59:43 -0600

    Ultrasonic Sensor For Collision Prevention
    - Convert Time To Distance 
    - Added One Meter Detection Distance for Stopping

---
+++ sensor_signal/sensor_signal.ino
@@ -0,0 +1,21 @@
+const int TRIG = 9;
+const int ECHO = 2;
+const int STOP = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(STOP, OUTPUT);
+  analogWrite(TRIG, 128);
+  Serial.begin(9800);
+ // attachInterrupt(digitalPinToInterrupt(2), IR2, CHANGE); we will just use pulsein because we are integrating one sensor only 
+}
+
+void loop() {
+  float pulse = pulseIn(ECHO, HIGH, 15000)*170/10000; // Convert pulse width to distance in cm
+  if(pulse < 100 && pulse != 0) // 100 cm is 1m, also ignore timeout values of 0 since the signal echo did not return 
+    digitalWrite(STOP, HIGH);
+  else
+    digitalWrite(STOP, LOW);
+}
