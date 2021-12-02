65f38bc813ba058b1d8694bada90528167494cbb
David Jochems <david.jochems@jci.com>
2021-12-02 17:24:08 -0600

    increase max speed

--- input_signal/input_signal.ino
+++ input_signal/input_signal.ino
@@ -1,10 +1,13 @@
 volatile long unsigned tchannel_2[4] = {0, 1500, 1500, 0}; // 1500uS is the neutral position, volatile is necessary for the interrupt
 volatile long unsigned tchannel_3[4] = {0, 1500, 1500, 0}; 
 volatile long unsigned tchannel_6[3] = {0, 1000, 1000}; // 1000 motor off 2000 motor on 
+long unsigned prev2 = 1500, prev3 = 1500;
+
 float weight = 0.99;
 int defaultval;
-#define forwardCap 1875  //  75% speed 
-#define backwardCap 1125 //  75% speed 
+#define forwardCap 1850  // forward speed cap
+#define backwardCap 1150 // backward speed cap
+#define deadzone 25
 
 void setup() {
   
@@ -53,33 +56,65 @@ void loop() {
     openRelay(); // Deadzone - David Jochems 
   }
 
-  /*Smoothing Operation On Output*/
+/*Smoothing Operation On Output*/
   if (tchannel_2[3])
-    tchannel_2[2] = tchannel_2[2] * weight + (tchannel_2[1] - tchannel_2[0]) * (1 - weight);
+  {
+//    if (tchannel_2[1] - tchannel_2[0] <= forwardCap && tchannel_2[1] - tchannel_2[0] >= backwardCap)
+//    {
+      if (abs(tchannel_2[1] - tchannel_2[0] - 1500) >= abs(tchannel_3[2] - 1500)) // accelerating
+        tchannel_2[2] = tchannel_2[2] * weight + (tchannel_2[1] - tchannel_2[0]) * (1 - weight);
+      else // deccelerating
+        tchannel_2[2] = tchannel_2[1] - tchannel_2[0];
+//    }
+  }
   if (tchannel_3[3])
-    tchannel_3[2] = tchannel_3[2] * weight + (tchannel_3[1] - tchannel_3[0]) * (1 - weight);
+  {
+//    if (tchannel_3[1] - tchannel_3[0] <= forwardCap && tchannel_3[1] - tchannel_3[0] >= backwardCap)
+//    {
+      if (abs(tchannel_3[1] - tchannel_3[0] - 1500) >= abs(tchannel_3[2] - 1500))
+        tchannel_3[2] = tchannel_3[2] * weight + (tchannel_3[1] - tchannel_3[0]) * (1 - weight);
+      else
+        tchannel_3[2] = tchannel_3[1] - tchannel_3[0];
+//    }
+  }
 
   /*Decrease Processor Performance*/
   Serial.print(tchannel_2[2]);
   Serial.println(tchannel_3[2]);
   
-  /*correcting out-of-bounds values*/
-  if (tchannel_2[2] > forwardCap) tchannel_2[2] = forwardCap;
-  if (tchannel_2[2] < backwardCap) tchannel_2[2] = backwardCap;
-  if (tchannel_3[2] > forwardCap) tchannel_3[2] = forwardCap;
-  if (tchannel_3[2] < backwardCap) tchannel_3[2] = backwardCap;
-      
   /* rounding - if sticks are close to each other, make them the same value (the avg)*/
-  if (tchannel_2[2] - tchannel_3[2] < 100 && tchannel_2[2] - tchannel_3[2] > -100)
-  {
-    int avg = (tchannel_2[2] + tchannel_3[2]) / 2;
-    tchannel_2[2] = avg;
-    tchannel_3[2] = avg;
-  }
-  
+//  if (tchannel_2[2] - tchannel_3[2] < 100 && tchannel_2[2] - tchannel_3[2] > -100)
+//  {
+//    int avg = (tchannel_2[2] + tchannel_3[2]) / 2;
+//    tchannel_2[2] = avg;
+//    tchannel_3[2] = avg;
+//  }
+
+  /*correcting out-of-bounds values*/
+  if (tchannel_2[2] > forwardCap) tchannel_2[2] = prev2;
+  if (tchannel_2[2] < backwardCap) tchannel_2[2] = prev2;
+  if (tchannel_3[2] > forwardCap) tchannel_3[2] = prev3;
+  if (tchannel_3[2] < backwardCap) tchannel_3[2] = prev3;
+
+  prev2 = tchannel_2[2];
+  prev3 = tchannel_3[2];
+
+  int rightMotor, leftMotor;
   /* Driving Functionality*/ 
-  int rightMotor = map(tchannel_2[2], 1000, 2000, 255, 128); // Mapping - 1000 to 2000 to 128 to 255 - David and Braydon 
-  int leftMotor  = map(tchannel_3[2], 1000, 2000, 128, 255);
+  if (tchannel_2[2] > 1500 + deadzone)
+    rightMotor = map(tchannel_2[2], 1500 + deadzone, 2000, 192, 128);
+  else if (tchannel_2[2] < 1500 - deadzone)
+    rightMotor = map(tchannel_2[2], 1000, 1500 - deadzone, 255, 192);
+  else
+    rightMotor = map(1, 0, 2, 255, 128);
+
+  if (tchannel_3[2] > 1500 + deadzone)
+    leftMotor = map(tchannel_3[2], 1500 + deadzone, 2000, 192, 255);
+  else if (tchannel_3[2] < 1500 - deadzone)
+    leftMotor = map(tchannel_3[2], 1000, 1500 - deadzone, 128, 192);
+  else
+    leftMotor = map(1, 0, 2, 128, 255);
+    
   bool Stop = digitalRead(6);
 
   /*Drive Motors And Prevent Forward Collision*/
@@ -115,6 +150,12 @@ void IR3() {
     tchannel_3[1] = micros();
     tchannel_3[3] = 1; // ready for calculation 
   }
+}
+
+volatile long unsigned safe_range( volatile long unsigned a[]) {   
+  if(a[1] - a[0] > forwardCap || a[1] - a[0] < backwardCap) // Overflow had nothing to do with wrong value being returned - Braydon 
+      return a[2];                            // Reuse previous value to drive motor 
+  return a[1] - a[0];                         // No problem with the difference, return it 
 }
 
 void closeRelay(){
